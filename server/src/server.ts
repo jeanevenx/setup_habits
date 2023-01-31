14609b0615696401e20e4189dfb44749946c52f4
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-01-30 23:13:15 -0300

    Making the ORM configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
 
 import {PrismaClient} from '@prisma/client'
 
 const app = Fastify();
 
+const prisma = new PrismaClient();
+
+app.register(cors);
+
 app.get('/', () => {
-    return 'Started';
+    const habits = prisma.habit.findMany();
+
+    return habits;
 })
 
 
